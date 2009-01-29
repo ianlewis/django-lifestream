29ebb8698e30a518d4e76ed3d0abf4b205e27894
Ian Lewis <IanMLewis@gmail.com>
2009-01-29 19:58:00 +0900

    Removed batch since you can use the update_feeds command

--- batch/update_feeds.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#:coding=utf-8:
-#:tabSize=2:indentSize=2:noTabs=true:
-#:folding=explicit:collapseFolds=1:
-
-from dlife.lifestream.feeds import update_feeds
-
-if __name__ == "__main__":
-  update_feeds()
