0294c374ae368dd4066adce3b1efd00cbb74332a
Ian Lewis <IanMLewis@gmail.com>
2009-09-02 22:38:53 +0900

    Removed extraneous template tags and filters

--- lifestream/templatetags/lifestream_tags.py
+++ lifestream/templatetags/lifestream_tags.py
@@ -11,10 +11,6 @@ from django.template.loader import render_to_string
 
 register = Library()
 
-@register.simple_tag
-def item_class(item):
-  return item.feed.domain.replace(".", "-") 
-
 class LifestreamItemNode(Node):
     def __init__(self, item_var):
         self.item_var = Variable(item_var)
@@ -56,8 +52,3 @@ def render_item_detail(parser, token):
     if len(bits) != 2:
         raise TemplateSyntaxError("%r takes one argument." % bits[0])
     return LifestreamItemDetailNode(bits[1])
-
-@register.filter
-def urlize_twitter(text):
-    import re
-    return mark_safe(re.sub(r'@([a-zA-Z0-9_]*)', r'@<a href="http://twitter.com/\1">\1</a>', text))
