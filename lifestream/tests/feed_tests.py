7f5a3db9774bd217d91c75a4ec70bee4c431f7bb
Ian Lewis <IanMLewis@gmail.com>
2009-10-10 01:31:24 +0900

    updated tests

--- lifestream/tests/feed_tests.py
+++ lifestream/tests/feed_tests.py
@@ -11,16 +11,34 @@ class RssFeedTest(BaseTest):
 
     def test_bitbucket_feed(self):
         update_feeds()
-        assert Item.objects.filter(feed__pk=1).count() == 15
+        self.assertEqual(Item.objects.filter(feed__pk=1).count(), 15)
+
+    #TODO: test fails.
+    def test_ianlewis_feed(self):
+        update_feeds()
+        self.assertEqual(Item.objects.filter(feed__pk=2).count(), 10)
 
 class AtomFeedTest(BaseTest):
     fixtures = ["atom.json"]
 
-    #TODO: test fails. feedparser is broken
+    #TODO: test fails.
     def test_bitbucket_atom_feed(self):
         update_feeds()
-        assert Item.objects.filter(feed__pk=2).count() == 15
+        self.assertEqual(Item.objects.filter(feed__pk=100).count(), 15)
 
     def test_youtube(self):
         update_feeds()
-        assert Item.objects.filter(feed__pk=3).count() == 25
+        self.assertEqual(Item.objects.filter(feed__pk=101).count(), 25)
+
+class RegressionTest(BaseTest):
+    fixtures = ["regressions.json"]
+
+    def assertNotEmpty(self, v):
+        self.assertNotEqual(v, None)
+        self.assertNotEqual(v, "")
+
+    def test_content(self):
+        update_feeds()
+        for item in Item.objects.filter(feed__pk=1):
+            self.assertNotEmpty(item.content)
+            self.assertNotEmpty(item.clean_content)
