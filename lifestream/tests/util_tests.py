01e829b3f61bc84dfc8c787615ce679b30037224
Ian Lewis <IanMLewis@gmail.com>
2009-10-10 16:52:41 +0900

    Added tests for css sanitization of style tags

--- lifestream/tests/util_tests.py
+++ lifestream/tests/util_tests.py
@@ -32,6 +32,39 @@ class EntitiesTest(HTMLSanitizationTest):
             u'<b>Ian&apos;s Homepage</b>',
         ),
         (
-            u'"I CaN HaZ SoMe <TeXt"', u'&quot;I CaN HaZ SoMe &lt;TeXt&gt;&quot;',
+            u'"I CaN HaZ SoMe <TeXt"',
+            u'&quot;I CaN HaZ SoMe &lt;TeXt&gt;&quot;',
+        ),
+    )
+
+class CSSSanitizationTest(HTMLSanitizationTest):
+    valid_tags = {
+        'div': ('style',),
+        'span': ('style',),
+    }
+    valid_styles = (
+        "color",
+        "font-weight",
+    )
+    test_html = (
+        (
+            u'<span style="color:#FFF;position:absolute;">My Homepage</span>', 
+            u'<span style="color:#FFF;">My Homepage</span>', 
+        ),
+        (
+            u'<span style="color:#FFF;position:absolute;font-weight:bold;">My Homepage</span>', 
+            u'<span style="color:#FFF;font-weight:bold;">My Homepage</span>', 
+        ),
+        (
+            u'<span style="color:#FFF;position:absolute   ">My Homepage</span>', 
+            u'<span style="color:#FFF;">My Homepage</span>', 
+        ),
+        (
+            u'<span style="  color:#FFF;  position:absolute;\tfont-weight:bold  ">My Homepage</span>', 
+            u'<span style="color:#FFF;font-weight:bold;">My Homepage</span>', 
+        ),
+        (
+            u'<span style="  color:#FFF;  position:absolute;\tfont-weight:bold;  aaaaa">My Homepage</span>', 
+            u'<span style="color:#FFF;font-weight:bold;">My Homepage</span>', 
         ),
     )
