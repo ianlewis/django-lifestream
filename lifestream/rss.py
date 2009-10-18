bdfd0c3df2e9d1ae51cf32be56865e9fe8dd8aa2
Ian Lewis <IanMLewis@gmail.com>
2009-10-18 10:57:00 +0900

    Fixed the feeds so that it works with multiple lifestreams and only tries to supply categories if the 'tagging' application is installed.

--- lifestream/rss.py
+++ lifestream/rss.py
@@ -24,22 +24,32 @@
 
 from django.contrib.syndication.feeds import Feed as SyndicationFeed
 from django.core.urlresolvers import reverse
+from django.conf import settings
+
 from lifestream.models import *
 
 class RecentItemsFeed(SyndicationFeed):
-  title = "Recent Items"
-  description = "Recent Lifestream Items"
+    title = "Recent Items"
+    description = "Recent Lifestream Items"
 
-  def link(obj):
-    return reverse('main_page')
+    def link(self, obj):
+        return reverse('lifestream_main_page', kwargs={
+            'lifestream_slug': obj.slug,
+        })
 
-  def items(self):
-    return Item.objects.published()[:10]
+    def get_object(self, bits):
+        return Lifestream.objects.get(slug=bits[0])
 
-  def item_pubdate(self, item):
-      return item.date
+    def items(self, obj):
+        return Item.objects.published()\
+                           .filter(feed__lifestream=obj)[:10]
 
-  def item_categories(self, item):
-      def item_categories(self, item):
-        return [tag.name for tag in item.tag_set]
-  
+    def item_pubdate(self, item):
+        return item.date
+
+    def item_categories(self, item):
+        def item_categories(self, item):
+            if 'tagging' in settings.INSTALLED_APPS:
+                return [tag.name for tag in item.tag_set]
+            else:
+                return []
