c925e99b8a29cfccffc8c5b3f651c99f64526007
Ian Lewis <IanMLewis@gmail.com>
2011-04-03 00:36:45 +0900

    Fixed Flickr plugin

--- lifestream/plugins/flickr.py
+++ lifestream/plugins/flickr.py
@@ -18,7 +18,8 @@ class FlickrPlugin(FeedPlugin):
         # Get the good resized image and put it in the media url
         # This is done by taking the thumbnail image url and removing
         # the "_s". No joke.
-        if ("media_thumbnail_attrs" in entry and
-                "url" in entry["media_thumbnail_attrs"]):
-            entry["media_content_attrs"]["url"] = (
-                entry["media_thumbnail_attrs"]["url"].replace("_s", ""))
+        if ('media_content' in entry and len(entry['media_content']) > 0 and
+              'media_thumbnail' in entry and len(entry['media_thumbnail']) > 0):
+            if 'url' in entry['media_thumbnail'][0]:
+                entry["media_content"][0]["url"] = (
+                    entry['media_thumbnail'][0]["url"].replace("_s", ""))
